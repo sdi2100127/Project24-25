0307a269507ad1c1c51497dfcd0f9dd9a76fac6b
sdi21000127 <sdi2100127@di.uoa.gr>
2024-10-19 12:46:05 +0300

    greedy search algorithm

---
+++ algorithms/algorithms.c
@@ -0,0 +1,80 @@
+#include "algorithms.h"
+
+Set* greedySearch(int** G, int nn, int vecs, int s, int xq, int k, int L, Set* V) {
+    // initializing the knn set with the starting point s
+    Set knn_set;
+    set_insert(knn_set, s);
+
+    // initializing the empty set for visited nodes
+    Set visited_nodes;
+
+    int flag = 1;
+
+    set_Node node_v;
+    int node_value, xp = -1;
+    int pos, min_pos;
+
+    while (flag == 1) {
+        // for every unvisited node in the knn_set
+        for (set_Node node = set_first(knn_set); node != set_last(knn_set); node = set_next(knn_set, node)) {
+            node_value = *set_node_value(knn_set, node);
+            if (set_find_node(visited_nodes, &node_value) == SET_EOF) {
+                // refer to the groundtruth graph to find the position of the unvisited node (node_value) in the column of the query node (xq)
+                // the smaller the position the smallest the euclidean distance between the unvisited node (node_value) and the query node (xq) 
+                for (int i=0; i<nn; i++) {
+                    if (G[i][xq] == node_value) {
+                        pos = i;
+                        break;
+                    }
+                }
+
+                // if the next node xp is not initialized yet initialize it with the first unvisited node you encounter
+                // otherwise check if its position is smaller than the current min_pos
+                if (xp == -1 || pos <= min_pos) {
+                    xp = node_value;
+                    min_pos = pos;
+                }   
+            }
+        }
+
+        // we need the outgoing neighbours of xp !!!!!!!!!!!
+
+        for (int i=0; i<nn; i++) {
+            // add all the neighbours of the node xp in the knn_set
+            set_insert(knn_set, G[i][xp]);
+        }
+
+        // add node xp to te visited_nodes set
+        set_insert(visited_nodes, xp);
+
+        int count = 0;
+        Set pruned_set;
+        // if the knn_set is bigger than L
+        if (set_size(knn_set) > L) {
+            // create a new set (pruned_set) and fill it with the L closest to xq nodes that are also in knn_set
+            for (int i=0; i<nn; i++) {
+                node_value = G[i][xq];
+                if (set_find_node(visited_nodes, &node_value) != SET_EOF) {
+                    set_insert(pruned_set, node_value);
+                    count++;
+                    if (count == L) break;
+                }
+            }
+        }
+
+        *V = visited_nodes;
+
+        flag = 0;
+
+        // each time check if all the nodes on the knn_set have been visited
+        for (set_Node node = set_first(knn_set); node != set_last(knn_set); node = set_next(knn_set, node)) {
+            node_value = *set_node_value(knn_set, node);
+            // if not the loop should continue
+            if (set_find_node(visited_nodes, &node_value) == SET_EOF) flag = 1;
+        }
+
+        return pruned_set;
+
+    }
+
+}
--- algorithms/algorithms.h
+++ algorithms/algorithms.h
@@ -1,4 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "Data_Structs/Data_Structs.h"
 
+// greedysearch is an algorithm that takes as input 
+// G --> the groundtruth graph
+// nn --> number of nearest neighbours / number of rows
+// vecs --> number of query vectors / number of columns
+// s --> a starting point in the graph
+// xq --> the query vector
+// k --> the result size
+// L --> search list size
+Set greedySearch(int** G, int nn, int vecs, int s, int xq, int k, int L, Set V);
